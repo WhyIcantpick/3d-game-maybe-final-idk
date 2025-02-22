9cb6ea960661292e4aa0d85d90c0bc7b915e2255
WhyIcantpick <161953637+WhyIcantpick@users.noreply.github.com>
2025-02-22 18:26:53 +0200

    Add files via upload

---
+++ панда 3д гра/3dgame.py
@@ -0,0 +1,17 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+from hero import Hero
+
+pos = (0, 0, 4)
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager(self)  # Передаємо об'єкт ShowBase до Mapmanager
+        self.camLens.setFov(90)  # Встановлюємо поле огляду камери
+        self.land.addBlock((0, 0, 0))
+        self.land.loadLand("land.txt")
+        self.hero = Hero(pos, self.land)
+
+game = Game()
+game.run()
